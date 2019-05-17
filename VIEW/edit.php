5f899d90a868bcca45248760ac2db6391a022bf9
ilhamfidatama <ilhamfidatama@gmail.com>
2019-05-17 12:57:52 +0800

    final update
    beberapa perbaikan dan perubahan tipe file

--- VIEW/edit.php
+++ VIEW/edit.php
@@ -1,3 +1,6 @@
+<?php  
+	include '../PHP/session.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,22 +9,25 @@
 	<link rel="stylesheet" type="text/css" href="CSS/style.css">
 </head>
 <body class="form">
+	<?php  
+		$nim = $_GET['nim'];
+		$nama = $_GET['nama'];
+	?>
 	<form class="logout" method="POST" action="../PHP/keluar.php">
 		<input type="submit" name="logout" class="red" value="Keluar">
 	</form>
-	<h1>Tambah data</h1>
+	<h1>Edit data</h1>
 	<div class="kotak_edit">
 		<p class="tulisan_editn"></p>
-		<form method="POST" action="../PHP/tambah.php">
+		<form method="POST" action="../PHP/edit.php">
 			<label>NIM</label>
-			<input type="text" name="nim" class="form_login" placeholder="nim">
+			<input type="text" name="nim" class="form_login" value="<?=$nim ?>">
 
 			<label>NAMA</label>
-			<input type="text" name="nama" class="form_login" placeholder="nama">
+			<input type="text" name="nama" class="form_login" value="<?=$nama ?>">
 
-			<input type="submit" class="tombol_edit" value="Tambah data">
+			<input type="submit" name="edit" class="tombol_edit" value="Edit">
 		</form>
-		
 	</div>
 </body>
 </html>
--- VIEW/tabel-data.php
+++ VIEW/tabel-data.php
@@ -1,3 +1,6 @@
+<?php  
+	include '../PHP/session.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +9,12 @@
 	<link rel="stylesheet" type="text/css" href="CSS/style.css">
 </head>
 <body>
-	<form class="logout" method="POST" action="">
+	<?php
+		include '../PHP/mahasiswa.php';
+
+		$data = getDataMahasiswa();
+	?>
+	<form class="logout" method="POST" action="../PHP/keluar.php">
 		<input type="submit" name="logout" class="red" value="Keluar">
 	</form>
 	<h1>tabel data</h1>
@@ -17,12 +25,15 @@
 			<th>NIM</th>
 			<th>Aksi</th>
 		</tr>
+		<?php $no = 1; while ($mahasiswa = mysqli_fetch_assoc($data)) : ?>
 		<tr>
-			<td>1</td>
-			<td>F1D016061</td>
-			<td>Mustiari</td>
+			<td><?=$no; ?></td>
+			<td><?=$mahasiswa['nim'] ?></td>
+			<td><?=$mahasiswa['nama'] ?></td>
 			<td>
-				<form class="formSiswa" method="POST" action="">
+				<form class="formSiswa" method="POST" action="../PHP/aksi-data-mahasiswa.php">
+					<input type="text" name="nim" hidden="true" value="<?=$mahasiswa['nim'] ?>">
+					<input type="text" name=" nama" hidden="true" value="<?=$mahasiswa['nama'] ?>">
 					<input type="submit" name="edit" class="blue" value="Edit">
 					<input type="submit" name="hapus" class="red" value="Hapus">
 				</form>
@@ -30,10 +41,12 @@
 				<a href="" class="red">Hapus</a> -->
 			</td>
 		</tr>
+		<?php $no++; ?>
+	<?php endwhile; ?>
 
 	</table>
 
-	<form class="kotak" method="POST" action="">
+	<form class="kotak" action="tambah.html">
 		<input type="submit" name="tambah" class="blue" value="Tambah Data Siswa">
 	</form>
 	
